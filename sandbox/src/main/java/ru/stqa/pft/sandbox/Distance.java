09517dd1e785685891e5c36062b554d0931cfecb
wooooooolf <mazafakinshit@yandex.ru>
2021-02-28 16:59:33 +0300

    lesson 2 (Distance between two points) second try

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -1,14 +1,15 @@
 package ru.stqa.pft.sandbox;
 
 public class Distance {
-
   public static void main(String[] args) {
 
-    Point P = new Point(1, 2, 3, 4);
-    System.out.println("points distance =" + distance(P));
+    Point1 P1 = new Point1(2, 2);
+
+    Point2 P2 = new Point2(3, 3);
+    System.out.println("points distance =" + distance(P1, P2));
   }
 
-  public static double distance(Point P) {
-    return Math.sqrt((P.p3 - P.p1) * (P.p3 - P.p1) + (P.p4 - P.p2) * (P.p4 - P.p2));
+  public static double distance(Point1 P1, Point2 P2) {
+    return Math.sqrt((P2.x - P1.x) * (P2.x - P1.x) + (P2.y - P1.y) * (P2.y - P1.y));
   }
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point2.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+public class Point2 {
+  public double x;
+  public double y;
+
+  public Point2 (double x, double y) {
+    this.x = x;
+    this.y = y;
+
+  }
+
+}
