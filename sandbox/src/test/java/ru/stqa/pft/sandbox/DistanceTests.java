9b3ce949b991528f58ee0863cde220ed1ac4972a
wooooooolf <mazafakinshit@yandex.ru>
2021-03-03 22:39:41 +0300

    edited Distance Test

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTests.java
@@ -8,11 +8,10 @@ public class DistanceTests {
   @Test
   public void testDistance() {
 
-    Distance D = new Distance();
-    Point1 P1 = new Point1(2, 2);
-    Point2 P2 = new Point2(3, 3);
+    Point P1 = new Point (1, 1);
+    Point P2 = new Point (2, 2);
 
-    Assert.assertEquals(D.distance(P1,P2),1.3142135623730951);
+    Assert.assertEquals(P1.distance(P2),1.3142135623730951);
 
   }
 }
