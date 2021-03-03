d3a1729c0addb34e1a5553063a9b6209cbb91b68
wooooooolf <mazafakinshit@yandex.ru>
2021-03-03 22:29:19 +0300

    edited Distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Distance.java
@@ -1,15 +1,17 @@
 package ru.stqa.pft.sandbox;
 
 public class Distance {
+
   public static void main(String[] args) {
 
-    Point1 P1 = new Point1(2, 2);
 
-    Point2 P2 = new Point2(3, 3);
-    System.out.println("points distance =" + distance(P1, P2));
-  }
+    Point p1 = new Point(1.0, 1.0);
+    Point p2 = new Point(2.0, 2.0);
 
-  public static double distance(Point1 P1, Point2 P2) {
-    return Math.sqrt((P2.x - P1.x) * (P2.x - P1.x) + (P2.y - P1.y) * (P2.y - P1.y));
+
+    System.out.println("Point 1 p1: x = " + p1.x + ", y = " + p1.y);
+    System.out.println("Point 2 p2: x = " + p2.x + ", y = " + p2.y);
+    System.out.println("Distance between p1 и p2 = " + p1.distance(p2));
+
   }
 }
