b07081ddc4b5407fdd81cbbc6f31bbd3fedf862d
wooooooolf <mazafakinshit@yandex.ru>
2021-03-08 14:25:28 +0300

    added HelperBase

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
@@ -1,8 +1,5 @@
 package ru.stqa.pft.adressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -26,24 +23,6 @@ public class ApplicationManager {
 
   public void stop() {
     wd.quit();
-  }
-
-  public boolean isElementPresent(By by) {
-    try {
-      wd.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  public boolean isAlertPresent() {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
   }
 
 
--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/SessionHelper.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/SessionHelper.java
@@ -3,22 +3,17 @@ package ru.stqa.pft.adressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class SessionHelper {
+public class SessionHelper extends HelperBase {
 
-  private WebDriver wd;
 
   public SessionHelper(WebDriver wd) {
 
-    this.wd = wd;
+    super(wd);
   }
 
   public void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
+    type(By.name("user"), username);
+    type(By.name("pass"), password);
+    click(By.xpath("//input[@value='Login']"));
   }
 }
