973503f03d31219e039e6669d5b7ddb2285b8cf0
wooooooolf <mazafakinshit@yandex.ru>
2021-03-14 19:23:42 +0300

    added possibility to run tests in Chrome and IE

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.adressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,9 +14,22 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    } else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
+    }
+
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/TestBase.java
@@ -1,18 +1,17 @@
 package ru.stqa.pft.adressbook.tests;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.adressbook.appmanager.ApplicationManager;
-import ru.stqa.pft.adressbook.model.UserData;
 
 public class TestBase {
 
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-  private WebDriver wd;
+  public WebDriver wd;
 
   @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
