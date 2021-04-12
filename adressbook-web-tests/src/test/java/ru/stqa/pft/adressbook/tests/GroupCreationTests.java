48ce6ed286ce3b8db661d1010bc8fc0b064605a9
wooooooolf <mazafakinshit@yandex.ru>
2021-04-12 12:03:35 +0300

    added DataProvider to Group and User CreationTests

--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/GroupCreationTests.java
@@ -1,21 +1,33 @@
 package ru.stqa.pft.adressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.adressbook.model.GroupData;
 import ru.stqa.pft.adressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new GroupData().withName("group").withHeader("header").withFooter("footer")});
+    list.add(new Object[]{new GroupData().withName("group1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[]{new GroupData().withName("group2").withHeader("header2").withFooter("footer2")});
+    return list.iterator();
+  }
 
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
 
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("group");
     app.group().create(group);
     Groups after = app.group().all();
     assertThat(after.size(), equalTo(before.size() + 1));
--- adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/UserCreationTests.java
+++ adressbook-web-tests/src/test/java/ru/stqa/pft/adressbook/tests/UserCreationTests.java
@@ -1,25 +1,38 @@
 package ru.stqa.pft.adressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import ru.stqa.pft.adressbook.model.GroupData;
 import ru.stqa.pft.adressbook.model.UserData;
 import ru.stqa.pft.adressbook.model.Users;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserCreationTests extends TestBase {
 
-
-  @Test
-  public void testUserCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validUsers(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new UserData().withName("Andrey").withSurname("Rublev").withPhone("21212121")
+            .withJob("Boring Company").withPhoto(new File("src/test/resources/12.jpg"))});
+    list.add(new Object[]{new UserData().withName("Andrey").withSurname("Rublev").withPhone("44444444")
+            .withJob("KroshkaKartoshka").withPhoto(new File("src/test/resources/13.jpg"))});
+    list.add(new Object[]{new UserData().withName("Andrey").withSurname("Rublev").withPhone("892888")
+            .withJob("Soyuzpechat").withPhoto(new File("src/test/resources/14.jpg"))});
+    return list.iterator();
+  }
+  @Test (dataProvider = "validUsers")
+  public void testUserCreation(UserData user){
 
     app.goTo().homePage();
     Users before = app.contact().all();
-    File photo = new File("src/test/resources/12.jpg");
-    UserData user = new UserData().withName("Andrey").withSurname("Rublev").withJob("Boring Company")
-            .withPhone("222111333").withEmail("rublev@rublev.com").withPhoto(photo);
+    //File photo = new File("src/test/resources/12.jpg");
     app.contact().create(user);
     assertThat(app.contact().count(),equalTo(before.size() + 1));
     Users after = app.contact().all();
