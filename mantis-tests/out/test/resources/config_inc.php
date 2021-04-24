38e590984f13d3b077c1e8b1c0b6df6e5bdf1f44
wooooooolf <mazafakinshit@yandex.ru>
2021-04-24 19:09:26 +0300

    added FtpHelper

---
+++ mantis-tests/out/test/resources/config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname = 'localhost';
+$g_db_type = 'mysqli';
+$g_database_name = 'bugtracker';
+$g_db_username = 'root';
+$g_db_password = '';
+$g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
+$g_default_timezone = 'Europe/Moscow';
+$g_default_language = "english";
+$g_crypto_master_salt = 'QxXTaoH+7R1GrhXYkL1mYZwhl4xxQpKNMAEwABdB+ps=';
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -18,6 +18,7 @@ public class ApplicationManager {
   private WebDriver wd;
   private String browser;
   private RegistrationHelper registrationHelper;
+  private FtpHelper ftp;
 
 
   public ApplicationManager(String browser) {
@@ -51,6 +52,13 @@ public class ApplicationManager {
       registrationHelper = new RegistrationHelper(this);
     }
     return registrationHelper;
+  }
+
+  public FtpHelper ftp() {
+    if (ftp == null){
+      ftp = new FtpHelper(this);
+    }
+      return ftp;
   }
 
   public WebDriver getDriver() {
