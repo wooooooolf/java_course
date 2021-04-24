ff95cf9fef4b9aaf17a0d142cca2b5bff4371405
wooooooolf <mazafakinshit@yandex.ru>
2021-04-24 22:12:38 +0300

    finished Registration Tests

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -24,6 +24,6 @@ public class RegistrationHelper extends HelperBase {
     wd.get(confirmationLink);
     type(By.name("password"), password);
     type(By.name("password_confirm"), password);
-    click(By.cssSelector("button[type='Update User']"));
+    click(By.cssSelector("button[type='submit']"));
   }
 }
